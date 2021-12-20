9a0f29e43c2faf474da91584e40b24ec12ecef90
João <joao.pdesa@hotmail.com>
2021-12-19 21:39:36 -0300

    first commit secound branch

--- functions.js
+++ functions.js
@@ -1,25 +1,25 @@
-const botao = document.getElementById('botao');
-const menu = document.getElementById('menu');
+// const botao = document.getElementById('botao');
+// const menu = document.getElementById('menu');
 
-botao.addEventListener('click', function(){
-    if(!menu.classList.contains('active')){
-        menu.classList.add('active');
+// botao.addEventListener('click', function(){
+//     if(!menu.classList.contains('active')){
+//         menu.classList.add('active');
 
-        let altura = menu.clientHeight + 'px';
-        menu.style.height = 0;
+//         let altura = menu.clientHeight + 'px';
+//         menu.style.height = 0;
 
-        setTimeout(function(){
-            menu.style.height = altura;
-        }, 0);
-    }
-    else{
-        menu.style.height = 0;
+//         setTimeout(function(){
+//             menu.style.height = altura;
+//         }, 0);
+//     }
+//     else{
+//         menu.style.height = 0;
 
-        menu.addEventListener('transitionend', function encolher(){
-            menu.classList.remove('active');
-            menu.style.height = 'auto';
+//         menu.addEventListener('transitionend', function encolher(){
+//             menu.classList.remove('active');
+//             menu.style.height = 'auto';
 
-            menu.removeEventListener('transitionend', encolher);
-        });
-    }
-});
+//             menu.removeEventListener('transitionend', encolher);
+//         });
+//     }
+// });
